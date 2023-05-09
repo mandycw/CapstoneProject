251c9924ba8849724b1214500f352388d3becad1
royal779 <121979294+royal779@users.noreply.github.com>
2023-05-09 15:30:31 -0700

    minor changes

--- Capstone Project/Assets/Scripts/Projectile.cs
+++ Capstone Project/Assets/Scripts/Projectile.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) { if (other.CompareTag("Enemy")) Destroy(gameObject);
+    if (other.CompareTag("Terrain")) Destroy(gameObject);
      }
+     
+     
 }
